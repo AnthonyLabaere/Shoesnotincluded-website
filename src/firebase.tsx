00750d1908d2f9b235adb6ebc710db2c3247da93
Anthony LABAERE <alabaere@santeclair.lan>
2022-03-14 14:39:41 +0100

    Ajout et configuration de firebase de dev

---
+++ src/firebase.tsx
@@ -0,0 +1,44 @@
+import { initializeApp } from 'firebase/app'
+import { collection, query, onSnapshot, where, getFirestore } from "firebase/firestore";
+
+import * as Types from './types'
+
+// https://firebase.google.com/docs/firestore/query-data/queries#web-version-9_2
+// https://travis.media/how-to-use-firebase-with-react/
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
+}
+
+const firebaseApp = initializeApp(firebaseConfig)
+
+const db = getFirestore(firebaseApp);
+
+export const subscribeToCities = (callback: (cities: Types.CityDocument[]) => void) => {
+  const unsubscribe = onSnapshot(collection(db, "cities"), (querySnapshot) => {
+    callback(querySnapshot.docs.map(doc => doc.data() as Types.CityDocument));
+  });
+
+  return unsubscribe;
+}
+
+export const subscribeToScenariosFromCity = (city: string, callback: (cities: Types.ScenarioSnapshot[]) => void) => {
+  const q = query(collection(db, "scenarii"), where("city", "==", city), where("active", "==", true));
+
+  const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    callback(querySnapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        data: doc.data() as Types.ScenarioDocument
+      }
+    }));
+  });
+
+  return unsubscribe;
+};
