098aa9323f659094b8a1eff9b344a6bfedf252a2
Anthony LABAERE <alabaere@santeclair.lan>
2022-04-05 10:43:18 +0200

    Plus d'informations sur la réduction

--- src/gui/screens/Prices/index.tsx
+++ src/gui/screens/Prices/index.tsx
@@ -18,7 +18,7 @@ const Prices = () => {
         </ContentPageContainer>
         <ContentPageContainer>
           <ContentContainer>
-            <h2 style={{ textAlign: 'center' }}>{Constants.WEBSITE_GAME_COST} pour une équipe de 1 à 5 joueurs.</h2>
+            <h2 style={{ textAlign: 'center' }}><span style={{ textDecoration: 'line-through' }}>{Constants.APP_GAME_COST}</span> {Constants.WEBSITE_GAME_COST} ({Constants.WEBSITE_TO_APP_GAME_REDUCTION_PERCENT}% de réduction en achetant directement sur le site) pour une équipe allant jusqu'à 5 joueurs.</h2>
             <h3 style={{ textAlign: 'center' }}>Ce prix est bien pour tout le groupe : ici le nombre de joueurs n'influe pas sur le prix de la partie.</h3>
             <h4 style={{ textAlign: 'center' }}>Pour plus d'informations, visitez la <StyledLink to="/faq">FAQ</StyledLink>.</h4>
             <Link style={{ display: 'flex', flex: 1, marginTop: 25 }} to="/achat"><Button style={{ flex: 1 }}>Je souhaite acheter un bon pour une partie</Button></Link>
